d18b09476a15e186b314dbbf71ce8d650d818a67
unknown <angelinajoliejoliejolie@yandex.ru>
2025-12-13 20:02:19 +0300

    Twelve pm2 commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,7 +139,7 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('http://api.mike395.nomorepartiessbs.ru');
+  const api = new Api('https://api.mike395.nomorepartiessbs.ru');
   
   export default api;
   
